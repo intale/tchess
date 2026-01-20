4a4f652e7496179d23c486e086323b63f06dfa6e
Ivan Dzyzenko <intale.a@gmail.com>
2026-01-20 11:30:48 +0100

    Make king and active_pieces properties "colored"

--- src/board_map.rs
+++ src/board_map.rs
@@ -2,29 +2,26 @@ use std::rc::Rc;
 use rustc_hash::{FxHashMap, FxHashSet};
 use crate::board_square::{BoardSquare};
 use crate::color::Color;
+use crate::colored_property::ColoredProperty;
 use crate::piece::Piece;
 use crate::point::Point;
 
 pub struct BoardMap {
     point_to_board_square: FxHashMap<Point, BoardSquare>,
-    active_white_pieces: FxHashSet<Rc<Piece>>,
-    active_black_pieces: FxHashSet<Rc<Piece>>,
-    white_king: Option<Rc<Piece>>,
-    black_king: Option<Rc<Piece>>,
+    active_pieces: ColoredProperty<FxHashSet<Rc<Piece>>>,
+    king: ColoredProperty<Option<Rc<Piece>>>,
 }
 
 impl BoardMap {
     pub fn empty() -> Self {
         Self {
             point_to_board_square: FxHashMap::default(),
-            active_white_pieces: FxHashSet::default(),
-            active_black_pieces: FxHashSet::default(),
-            white_king: None,
-            black_king: None,
+            active_pieces: ColoredProperty([FxHashSet::default(), FxHashSet::default()]),
+            king: ColoredProperty([None, None]),
         }
     }
 
-    pub fn board_square(&'_ self, point: &Point) -> &BoardSquare {
+    pub fn board_square(&self, point: &Point) -> &BoardSquare {
         self.point_to_board_square.get(point).unwrap_or(&BoardSquare::VoidSquare)
     }
 
@@ -54,15 +51,11 @@ impl BoardMap {
         square.set_piece_rc(&piece);
         piece.set_current_position(*point);
         if add_to_active_pieces {
-            self.active_pieces_mut(piece.color()).insert(Rc::clone(piece));
+            self.active_pieces[piece.color()].insert(Rc::clone(piece));
 
             match &**piece {
                 Piece::King(_) => {
-                    match piece.color() {
-                        Color::White => self.white_king = Some(Rc::clone(piece)),
-                        Color::Black => self.black_king = Some(Rc::clone(piece)),
-                    }
-
+                    self.king[piece.color()] = Some(Rc::clone(piece));
                 },
                 _ => ()
             }
@@ -89,13 +82,10 @@ impl BoardMap {
             };
 
         if let Some(piece) = square.remove_piece() && remove_from_active_pieces {
-            self.active_pieces_mut(piece.color()).remove(&piece);
+            self.active_pieces[piece.color()].remove(&piece);
             match &*piece {
                 Piece::King(_) => {
-                    match piece.color() {
-                        Color::White => self.white_king = None,
-                        Color::Black => self.black_king = None,
-                    }
+                    self.king[piece.color()] = None;
                 },
                 _ => ()
             }
@@ -103,23 +93,10 @@ impl BoardMap {
     }
 
     pub fn king(&self, color: &Color) -> Option<&Rc<Piece>> {
-        match color {
-            Color::White => self.white_king.as_ref(),
-            Color::Black => self.black_king.as_ref(),
-        }
+        self.king[color].as_ref()
     }
 
     pub fn active_pieces(&self, color: &Color) -> &FxHashSet<Rc<Piece>> {
-        match color {
-            Color::White => &self.active_white_pieces,
-            Color::Black => &self.active_black_pieces,
-        }
-    }
-
-    fn active_pieces_mut(&mut self, color: &Color) -> &mut FxHashSet<Rc<Piece>> {
-        match color {
-            Color::White => &mut self.active_white_pieces,
-            Color::Black => &mut self.active_black_pieces,
-        }
+        &self.active_pieces[color]
     }
 }
