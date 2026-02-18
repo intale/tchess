4eaff1c5aa6e1cc180e22d18f5c5f2a62a295d72
Ivan Dzyzenko <intale.a@gmail.com>
2026-02-18 11:38:06 +0100

    Remove unused file

--- libtchess/src/board_square_builder.rs
+++
@@ -1,9 +0,0 @@
-pub mod default_square_builder;
-
-use crate::board_square::BoardSquare;
-use crate::point::Point;
-
-pub trait BoardSquareBuilder {
-    fn init() -> Self where Self: Sized;
-    fn build(&self, point: &Point) -> Option<BoardSquare>;
-}
